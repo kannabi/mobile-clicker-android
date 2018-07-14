38e617e51ad44029588edc6079d514debe24b157
Valentin Schukin <hekpoxuoypr@gmail.com>
2018-07-14 15:58:33 +0700

    feat(connection): move a ConnectionManager creating to a dagger module

--- app/src/main/java/com/awsm_guys/mobileclicker/di/ConnectionModule.kt
+++ app/src/main/java/com/awsm_guys/mobileclicker/di/ConnectionModule.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import com.awsm_guys.mobileclicker.connection.IConnectionModel
 import com.awsm_guys.mobileclicker.connection.IConnectionPresenter
 import com.awsm_guys.mobileclicker.connection.model.ConnectionModel
+import com.awsm_guys.mobileclicker.connection.model.manager.ConnectionManager
+import com.awsm_guys.mobileclicker.connection.model.manager.lan.LanConnectionManager
 import com.awsm_guys.mobileclicker.connection.presenter.ConnectionPresenter
 import com.awsm_guys.mobileclicker.di.scopes.ConnectionScope
 import com.awsm_guys.mobileclicker.primitivestore.PrimitiveStore
@@ -17,6 +19,11 @@ class ConnectionModule {
             ConnectionPresenter(model, context)
 
     @ConnectionScope @Provides
-    fun provideModel(primitiveStore: PrimitiveStore, context: Context): IConnectionModel =
-            ConnectionModel(primitiveStore, context)
+    fun provideModel(
+            primitiveStore: PrimitiveStore, context: Context, connectionManager: ConnectionManager
+    ): IConnectionModel =
+            ConnectionModel(primitiveStore, context, connectionManager)
+
+    @ConnectionScope @Provides
+    fun provideConnectionManager() = LanConnectionManager() as ConnectionManager
 }
