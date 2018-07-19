cac7f1fc47226bb5f3d43db04b74c133464b7adb
Valentin Schukin <hekpoxuoypr@gmail.com>
2018-07-19 22:51:05 +0700

    feat(tests): add RxSocketWrapper test

--- app/src/main/java/com/awsm_guys/mobileclicker/utils/Utils.kt
+++ app/src/main/java/com/awsm_guys/mobileclicker/utils/Utils.kt
@@ -2,8 +2,11 @@ package com.awsm_guys.mobileclicker.utils
 
 import android.view.View
 import com.jakewharton.rxbinding2.view.RxView
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.functions.BiConsumer
+import io.reactivex.schedulers.Schedulers
 import java.util.concurrent.TimeUnit
 
 fun wrapClickListening(compositeDisposable: CompositeDisposable,
@@ -13,5 +16,18 @@ fun wrapClickListening(compositeDisposable: CompositeDisposable,
                     .throttleWithTimeout(150, TimeUnit.MILLISECONDS)
                     .observeOn(AndroidSchedulers.mainThread())
                     .subscribe(onNext)
+    )
+}
+
+fun makeSingle(compositeDisposable: CompositeDisposable, body: (() -> Unit)) {
+    compositeDisposable.add(
+            Single.fromCallable(body).subscribeOn(Schedulers.io()).subscribe()
+    )
+}
+
+fun <T> makeSingle(compositeDisposable: CompositeDisposable, body: (() -> Unit),
+                   onCallback: BiConsumer<in Any, in Throwable>) {
+    compositeDisposable.add(
+            Single.fromCallable(body).subscribeOn(Schedulers.io()).subscribe(onCallback)
     )
 }
